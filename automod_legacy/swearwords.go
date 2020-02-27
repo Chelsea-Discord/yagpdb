b62f9688862f158213adc52e6b1fcec7db911297
Chelsea-Discord <61534207+Chelsea-Discord@users.noreply.github.com>
2020-02-27 07:26:59 +0100

    Update swearwords.go

--- automod_legacy/swearwords.go
+++ automod_legacy/swearwords.go
@@ -73,4 +73,7 @@ var BuiltinSwearWords = map[string]bool{
 	"asshat":      true,
 	"pu55y":       true,
 	"pen1s":       true,
+	"PornHub":     true,
+	"Tf":          true,
+	"Tik":         true,
 }
